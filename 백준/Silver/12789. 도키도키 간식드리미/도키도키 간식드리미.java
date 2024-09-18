1ab203e3877cc20865cd1b2d6d634afa6097e4ac
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2024-09-18 20:54:53 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -17,45 +17,50 @@ public class Main {
         Stack<Integer> stackInt = new Stack<>();
         for (int i = 0; i < N; i++) {
             currentNum = Integer.parseInt(strtk.nextToken());
-            //System.out.println("\n\n현재 입력받은 값: "+currentNum);
+//            System.out.println("\n\n현재 입력받은 값: "+currentNum);
 
             if(currentNum == nextSeq) {
                 nextSeq++;
-                //System.out.println(currentNum+"을 바로 내보냄.");
+//                System.out.println(currentNum+"을 바로 내보냄.");
             } else if(stackInt.isEmpty()) {
-                //System.out.println("비어있던 스택에 들어간 값: "+currentNum);
+//                System.out.println("비어있던 스택에 들어간 값: "+currentNum);
                 stackInt.push(currentNum);
                 maxNumInStack = currentNum;
-                //System.out.println("현재 스택 내의 가장 큰 값: "+maxNumInStack);
+//                System.out.println("현재 스택 내의 가장 큰 값: "+maxNumInStack);
             } else if(stackInt.peek() == nextSeq) {
                 while(!stackInt.isEmpty()) {
                     if(stackInt.peek() == nextSeq){
-                        //System.out.println(stackInt.pop()+"이 스택에 있다가 내보내짐.");
+//                        System.out.println(stackInt.pop()+"이 스택에 있다가 내보내짐.");
                         stackInt.pop();
                         nextSeq++;
                     } else {
                         break;
                     }
                 }
+
                 if(stackInt.isEmpty()) {
                     maxNumInStack = currentNum;
                 }
-                if (currentNum > maxNumInStack) {
-                    //System.out.println("Break!!!!!!");
-                    //System.out.println("스택에 넣으려는 "+currentNum+"이 "+maxNumInStack+"보다 크다.");
+
+                if(currentNum == nextSeq) {
+//                    System.out.println(currentNum+"을 바로 내보냄.");
+                    nextSeq++;
+                } else if (currentNum > maxNumInStack) {
+//                    System.out.println("Break!!!!!!");
+//                    System.out.println("스택에 넣으려는 "+currentNum+"이 "+maxNumInStack+"보다 크다.");
                     isSafe = false;
                     break;
                 } else {
-                    //System.out.println("Stack에 값이 들어감: "+currentNum);
+//                    System.out.println("Stack에 값이 들어감: "+currentNum);
                     stackInt.push(currentNum);
                 }
             } else if (currentNum > maxNumInStack) {
-                //System.out.println("Break!!!!!!");
-                //System.out.println("스택에 넣으려는 "+currentNum+"이 "+maxNumInStack+"보다 크다.");
+//                System.out.println("Break!!!!!!");
+//                System.out.println("스택에 넣으려는 "+currentNum+"이 "+maxNumInStack+"보다 크다.");
                 isSafe = false;
                 break;
             } else {
-                //System.out.println("Stack에 값이 들어감: "+currentNum);
+//                System.out.println("Stack에 값이 들어감: "+currentNum);
                 stackInt.push(currentNum);
             }
         }
