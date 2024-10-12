052a15853f86aed6016ffe7e8fba1ef26f97ecd6
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2024-10-12 15:46:36 +0900

    [Silver III] Title: 주유소, Time: 372 ms, Memory: 36096 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -13,40 +13,34 @@ public class Main {
         // 도시의 수
         int N = Integer.parseInt(br.readLine());
 
-        int[] road = new int[N-1];
-        int[] cost = new int[N];
+        long[] road = new long[N-1];
+        long[] cost = new long[N];
 
         // 유류비 가장 싼곳에 도착했을 때 사용하기 위한 남은 총 도로의 길이
         long lengthLeft = 0;
 
         // 각 도시를 잇는 도로의 길이
         StringTokenizer strtk1 = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < road.length; i++) {
+        for (int i = 0; i < N-1; i++) {
             road[i] = Integer.parseInt(strtk1.nextToken());
             lengthLeft += road[i];
         }
 
-        int minCost = 0;
         StringTokenizer strtk2 = new StringTokenizer(br.readLine(), " ");
-        for (int j = 0; j < cost.length; j++) {
+        for (int j = 0; j < N-1; j++) {
             cost[j] = Integer.parseInt(strtk2.nextToken());
-            if(j == 0) {
-                minCost = cost[j];
-            } else if(cost[j] < minCost && j != (cost.length-1)) {
-                minCost = cost[j];
-            }
         }
 
-        long result = 0;
-        for (int k = 0; k < N-1; k++) {
-            if(cost[k] != minCost) {
-                result += road[k]*cost[k];
-                lengthLeft -= road[k];
-            } else if(cost[k] == minCost) {
-                result += cost[k]*lengthLeft;
-                break;
+
+        long minCost = cost[0];
+        long result = road[0] * cost[0];
+        for (int k = 1; k < N-1; k++) {
+            // minCost == 현재 가본 도시들 중에서, 가장 저렴한 유류비를 기록한 변수
+            if(cost[k] < minCost) {
+                minCost = cost[k];
             }
+            result += minCost * road[k];
         }
-        System.out.println(result);
+        System.out.print(result);
     }
 }
