f8124988a74576d0ce79064e55301d5541350f1a
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2025-07-10 15:18:23 +0900

    [level 2] Title: 교점에 별 만들기, Time: 31.96 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,86 +1,100 @@
 import java.util.*;
-//좌표 클래스
-class Coordinate {
+
+class Node {
     long x;
     long y;
-    
-    Coordinate(long x, long y){
+    public Node(long x, long y) {
         this.x = x;
         this.y = y;
     }
     
-    void setX(long x){
-        this.x = x;
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
-    
-    void setY(long y){
-        this.y = y;
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Node) {
+            Node o = (Node) obj;
+            return o.x == x && o.y == y;
+        } else
+            return false;
     }
 }
 
 class Solution {
     public String[] solution(int[][] line) {
-        //교점을 저장할 좌표 리스트
-        List<Coordinate> cList = new ArrayList<>();
-        //교점 저장하기
-        for(int i = 0; i < line.length - 1; i++){
-            for(int j = i + 1; j < line.length; j++){
-
-                long a = line[i][0], b = line[i][1], e = line[i][2],
-                     c = line[j][0], d = line[j][1], f = line[j][2];
+        HashSet<Node> set = new HashSet<>(); // 별 위치 저장하는 set
+        // min, max 좌표값
+        long maxX = Long.MIN_VALUE, minX = Long.MAX_VALUE;
+        long maxY = Long.MIN_VALUE, minY = Long.MAX_VALUE;
+        
+        
+        // 모든 경우의 수의 두 방정식 조합에서 x, y값을 구한 뒤 0이 아닌 정수라면 set에 저장
+        // a, b, c, d, e, f는 x, y값 계산에 필요한 변수
+        // parent는 두개의 계산식에 공통적으로 사용되는 값
+        long a, b, c, d, e, f, parent;
+        for(int i = 0; i < line.length; i++) {
+            int[] cur = line[i]; // 방정식 1
+            a = cur[0];
+            b = cur[1];
+            e = cur[2];
+            
+            for(int j = i+1; j < line.length; j++) {
+                int[] target = line[j]; // 방정식 2
+                c = target[0];
+                d = target[1];
+                parent = (a*d) - (b*c);
+                if(parent == 0) {
+                    continue;
+                }
                 
-                if((a * d - b * c) != 0){
-                    double x = (double) (b * f - e * d) / (a * d - b * c);
-                    double y = (double) (e * c - a * f) / (a * d - b * c);
-                    if(x % 1 == 0 && y % 1 == 0){                           //정수인 경우에만 저장
-                        cList.add(new Coordinate((long)x, (long)y));   
-                    }
+                f = target[2];
+                
+                // x, y를 구한다.
+                double x = (double) ((b*f) - (e*d)) / parent;
+                double y = (double) ((e*c) - (a*f)) / parent;
+                
+                if(x % 1.0 != 0.0 || y % 1.0 != 0.0) {
+                    continue;
+                } else {
+                    // 0이 아닌 정수인 경우 set에 추가한다.
+                    long tmpX = (long) x;
+                    long tmpY = (long) y;
+                    
+                    // x, y의 최대값 및 최소값을 갱신한다.(결과값 출력을 위함)
+                    if(tmpX > maxX)
+                        maxX = tmpX;
+                    if(tmpX < minX)
+                        minX = tmpX;
+                    if(tmpY > maxY)
+                        maxY = tmpY;
+                    if(tmpY < minY)
+                        minY = tmpY;
+                    
+                    set.add(new Node(tmpX, tmpY));
                 }
             }
         }
-        //교점의 최소, 최대값 찾기
-        Coordinate min = new Coordinate(Long.MAX_VALUE, Long.MAX_VALUE);
-        Coordinate max = new Coordinate(Long.MIN_VALUE, Long.MIN_VALUE);
         
-        for(Coordinate c : cList){
-            long cx = c.x, cy = c.y;
-            
-            if(cx < min.x){
-                min.setX(cx);
-            }
-            
-            if(cy < min.y){
-                min.setY(cy);
-            }
-            
-            if(cx > max.x){
-                max.setX(cx);
-            }
-            
-            if(cy > max.y){
-                max.setY(cy);
-            }
-        }
+        int width = (int) (maxX - minX + 1);
+        int height = (int) (maxY - minY + 1);
         
-        //격자판 크기 산출 후 격자판 배열 선언
-        int width = (int)(max.x - min.x + 1);
-        int height = (int)(max.y - min.y + 1);
-            
-        char[][] arr = new char[height][width];
-        
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++){
-                arr[i][j] = '.';
-            }           
-        }
-        //교점 찍기
-        for(Coordinate c : cList){
-            arr[(int)(max.y - c.y)][(int)(c.x - min.x)] = '*';
-        }
-        //답변배열 생성 후 격자판 문자열로 변환하여 저장
         String[] answer = new String[height];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = new String(arr[i]);
+        char[][] rows = new char[height][width];
+        
+        for(int i = 0; i < rows.length; i++) {
+            Arrays.fill(rows[i], '.');
+        }
+        
+        for(Node cur : set) {
+            int y = (int) (cur.y - maxY) * -1;
+            int x = (int) (cur.x - minX);
+            rows[y][x] = '*';
+        }
+        
+        for(int i = 0; i < rows.length; i++) {
+            answer[i] = new String(rows[i]);
         }
         
         return answer;
