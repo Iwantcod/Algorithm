30d85aa505a2e8f242bece0b6ca672b056e68510
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2025-07-25 10:48:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 736 ms, Memory: 69400 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,33 +1,42 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
+class Node implements Comparable<Node> {
+    int x, y;
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if(o.x > this.x) {
+            return -1;
+        } else if(o.x < this.x) {
+            return 1;
+        } else {
+            return this.y - o.y;
+        }
+    }
+}
+
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] ary = new int[N][2];
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Node> q = new PriorityQueue<>();
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer strtk = new StringTokenizer(br.readLine(), " ");
-            ary[i][0] = Integer.parseInt(strtk.nextToken());
-            ary[i][1] = Integer.parseInt(strtk.nextToken());
+        for(int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            q.add(new Node(Integer.parseInt(s[0]), Integer.parseInt(s[1])));
         }
 
-
-        // JAVA 2차원 배열 LAMBDA식 이용 오름차순 정렬
-        Arrays.sort(ary, (o1, o2) -> {
-            if(o1[0] == o2[0]) {    // 0번째 숫자가 같은 경우, 1번째 숫자를 비교하여 오름차순 정렬
-                return o1[1]-o2[1];
-            } else {
-                return o1[0]-o2[0];
-            }
-        });
-
-        for (int i = 0; i < N; i++) {
-            bw.write(String.valueOf(ary[i][0])+" "+String.valueOf(ary[i][1])+"\n");
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            bw.write(String.valueOf(cur.x) + " " + String.valueOf(cur.y) + "\n");
         }
         bw.close();
     }
