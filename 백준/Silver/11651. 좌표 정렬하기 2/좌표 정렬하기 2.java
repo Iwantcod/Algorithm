dc8118936b6a04c5d7c733565fc7f496d4d257f5
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2025-07-25 10:49:24 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 752 ms, Memory: 68572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node implements Comparable<Node> {
+    int x, y;
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if(o.y > this.y) {
+            return -1;
+        } else if(o.y < this.y) {
+            return 1;
+        } else {
+            return this.x - o.x;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Node> q = new PriorityQueue<>();
+
+        for(int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            q.add(new Node(Integer.parseInt(s[0]), Integer.parseInt(s[1])));
+        }
+
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            bw.write(String.valueOf(cur.x) + " " + String.valueOf(cur.y) + "\n");
+        }
+        bw.close();
+    }
+}
