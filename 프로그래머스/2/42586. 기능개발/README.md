b81afbdd399cc6620d59d77f2c357156dc284f1e
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2025-09-13 13:42:06 +0900

    [level 2] Title: 기능개발, Time: 0.29 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 85.5 MB, 시간: 0.32 ms
+메모리: 72.3 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 13일 13:31:58
+2025년 09월 13일 13:42:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,26 +1,33 @@
 // 13:10 시작
+// 13:36 종료
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        // 첫번째 작업을 완료시킨 다음 이후의 작업을 계산
+        // 작업을 완료하기 위해 소요된 기간(첫번째 작업 기준으로 초기화)
         int day = (int)Math.ceil((double)(100 - progresses[0]) / speeds[0]);
         if(progresses.length == 1) {
             return new int[]{day};
         }
-        int cnt = 1;
+        int cnt = 1; // 각 배포마다 배포되는 현재까지 완료된 작업의 개수. 첫번째 작업을 완료했다 가정하고 1로 초기화
         List<Integer> list = new ArrayList<>();
         
+        // 두번째 작업부터 마지막 작업까지 순회
         for(int i = 1; i < progresses.length; i++) {
+            // 현재 작업의 남은 작업량 계산
             int last = 100 - progresses[i] - (speeds[i] * day);
             if(last > 0) {
+                // 작업을 완료할 수 없다면 이때까지 완료된 작업을 배포
                 list.add(cnt);
+                // 현재 작업을 완료하기 위해 필요한 추가 기간 계산 후 완료처리
                 day += (int)Math.ceil((double)last / speeds[i]);
-                cnt = 1;
+                cnt = 1; // 완료처리한 현재 작업을 카운팅
             } else {
-                cnt++;
+                cnt++; // 작업을 완료할 수 있다면 카운팅
             }
         }
-        list.add(cnt);
+        list.add(cnt); // 루프가 종료된 이후 마지막 배포 수행
         
         int[] answer = new int[list.size()];
         for(int i = 0; i < list.size(); i++) {
