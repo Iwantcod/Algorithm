5f353734c08cb5caacdab2fcb100dc6d1c6e9fe9
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2025-09-18 13:14:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -6,28 +6,29 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
 
         if(n % 5 == 0) {
-            cnt = n/5;
-            n = 0;
-        } else {
-             while(n > 0) {
-                n -= 3;
-                cnt++;
-                if(n > 0 && n % 5 == 0) {
-                    cnt += n/5;
-                    n = 0;
-                }
+            bw.write(String.valueOf(n/5));
+            bw.close();
+            return;
+        }
+
+        int cnt5 = 0;
+        int cnt3 = 0;
+        cnt5 = n / 5;
+        n %= 5;
+        while(cnt5 >= 0) {
+            if(n % 3 == 0) {
+                cnt3 += n / 3;
+                break;
+            } else {
+                cnt5--;
+                n += 5;
             }
         }
-        if(n == 0) {
-            bw.write(String.valueOf(cnt));    
-        } else {
-            bw.write("-1");
-        }
+
+        bw.write(String.valueOf(cnt5 + cnt3));
         bw.close();
     }
 }
