8269c4fed6fd510c7683b104de55d9bb991ff1ef
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2025-12-22 13:10:39 +0900

    [level 3] Title: 금과 은 운반하기, Time: 135.89 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,45 +1,34 @@
+// 09:37
 class Solution {
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        long l = 0L;
-        long r = 1L;
-        while(check(a, b, g, s, w, t, r) == false) {
-            l = r;
-            r *= 2;
-        }
-        while(l < r) {
-            long mid = (l + r) / 2;
-            if(check(a, b, g, s, w, t, mid)) {
-                r = mid;
+        int n = t.length;
+        long p = 0;
+        long q = 400_000_000_000_000L;
+        long answer = 0;
+        while(p < q) {
+            long mid = (p+q)/2;
+            long totalG = 0;
+            long totalS = 0;
+            long total = 0;
+            for(int i = 0; i < n; i++) {
+                long time = mid/(t[i]*2);
+                if(mid % (t[i]*2) >= t[i]) {
+                    time++;
+                }
+                long curG = Math.min(g[i], w[i] * time);
+                long curS = Math.min(s[i], w[i] * time);
+                totalG += curG;
+                totalS += curS;
+                total += Math.min(g[i] + s[i], w[i] * time);
+            }
+            if(total >= a + b && totalG >= a && totalS >= b) {
+                answer = mid;
+                q = mid;
             } else {
-                l = mid + 1;
+                p = mid + 1;
             }
         }
         
-        return r;
-    }
-    public boolean check(int a, int b, int[] g, int[] s, int[] w, int[] t, long target) {
-        int n = w.length;
-        long total = 0L;
-        long totalG = 0L;
-        long totalS = 0L;
-        
-        for(int i = 0; i < n; i++) {
-            // 최대 왕복 횟수 구하기 -> 최대 운반 가능량 구하기
-            // 1. 해당 시간에 최대 운반 가능량 구하기
-            long cnt = target / (t[i] * 2);
-            if(target % (t[i] * 2) >= t[i]) {
-                cnt++;
-            }
-            long curMax = Math.min(cnt * w[i], g[i] + s[i]);
-            total += curMax;
-            // 2. 금만 옮기는 경우의 운반량 구하기
-            totalG += Math.min(curMax, g[i]);
-            // 3. 은만 옮기는 경우의 운반량 구하기
-            totalS += Math.min(curMax, s[i]);
-        }
-        if(total >= a + b && totalG >= a && totalS >= b) {
-            return true;
-        }
-        return false;
+        return answer;
     }
 }
