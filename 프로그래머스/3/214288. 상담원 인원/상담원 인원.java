c279e0604139d7164831e10c5e6861038cfcef8c
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2025-12-31 12:25:21 +0900

    [level 3] Title: 상담원 인원, Time: 41.87 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
@@ -1,15 +1,10 @@
-// 16:35
+// 11:54
 import java.util.*;
 class Solution {
     int[] kArr;
     int[][] reqs;
-    int answer;
+    int answer = Integer.MAX_VALUE;
     public int solution(int k, int n, int[][] reqs) {
-        this.kArr = new int[k+1];
-        this.answer = Integer.MAX_VALUE;
-        this.reqs = reqs;
-        Arrays.fill(kArr, 1);
-        n -= k;
         Arrays.sort(reqs, (r1, r2) -> {
             if(r1[2] == r2[2]) {
                 return r1[0] - r2[0];
@@ -17,57 +12,67 @@ class Solution {
                 return r1[2] - r2[2];
             }
         });
-        
+        this.reqs = reqs;
+        this.kArr = new int[k+1];
+        Arrays.fill(kArr, 1);
+        n -= k;
+        // for(int i = 0; i < reqs.length; i++) {
+        //     System.out.println(reqs[i][0]+" "+reqs[i][1]+" "+reqs[i][2]);
+        // }
         dfs(n, 1);
+
         return answer;
     }
     public void dfs(int n, int depth) {
+        if(n < 0) {
+            return;
+        }
         if(depth == kArr.length) {
             if(n == 0) {
                 answer = Math.min(answer, cal());
             }
             return;
         }
+        
         for(int i = 0; i <= n; i++) {
             kArr[depth] += i;
             dfs(n - i, depth + 1);
             kArr[depth] -= i;
         }
     }
+    
     public int cal() {
-        int cnt = 0;
         int indx = 0;
-        for(int i = 1; i < kArr.length; i++) {
-            int[] next = new int[kArr[i]];
-            while(indx < reqs.length) {
-                int[] cur = reqs[indx];
-                if(cur[2] != i) {
+        int kIndx = 1;
+        int[] kNext = new int[kArr[kIndx]];
+        int wait = 0;
+        while(indx < reqs.length) {
+            if(reqs[indx][2] > kIndx) {
+                kIndx++;
+                kNext = new int[kArr[kIndx]];
+            }
+            
+            boolean isFind = false;
+            int minIndx = 0;
+            int minVal = kNext[0];
+            for(int i = 0; i < kNext.length; i++) {
+                if(minVal > kNext[i]) {
+                    minIndx = i;
+                    minVal = kNext[i];
+                }
+                if(reqs[indx][0] >= kNext[i]) {
+                    kNext[i] = reqs[indx][0] + reqs[indx][1];
+                    isFind = true;
                     break;
                 }
-                indx++;
-                boolean isWait = true;
-                int minIndx = -1;
-                int minVal = Integer.MAX_VALUE;
-                for(int j = 0; j < next.length; j++) {
-                    if(next[j] < minVal) {
-                        // 대기시간 측정을 위한 '다음 상담 가능 시간이 가장 이른' 멘토 기록
-                        minIndx = j;
-                        minVal = next[j];
-                    }
-                    
-                    if(next[j] <= cur[0]) {
-                        // 대기시간이 필요없는 경우 해당 멘토의 '다음 상담 가능 시간'을 갱신
-                        isWait = false;
-                        next[j] = cur[0] + cur[1];
-                        break;
-                    }
-                }
-                if(isWait == true) {
-                    cnt += next[minIndx] - cur[0];
-                    next[minIndx] += cur[1];
-                }
             }
+            if(isFind == false) {
+                wait += kNext[minIndx] - reqs[indx][0];
+                kNext[minIndx] += reqs[indx][1];
+            }
+            indx++;
         }
-        return cnt;
+        return wait;
     }
+
 }
