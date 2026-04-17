a87b3432363e2c1c61e48b577ba21f5b3b577ddd
Joonha Kim <86515657+Iwantcod@users.noreply.github.com>
2026-04-17 09:53:54 +0900

    [Gold IV] Title: 스도쿠, Time: 1476 ms, Memory: 306324 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -2,85 +2,102 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// 09:07
 class Main {
     static int[][] map;
-    static List<int[]> emptyAddr;
-    static boolean isFinish;
+    static boolean isDone;
     public static void main(String[] args) throws IOException {
-        // 백트래킹 이용
-        // 1. 비어있는 좌표 정보 수집: emptyAddr
-        // 2. 비어있는 좌표마다 1~9를 순서대로 대입해보며, 스도쿠 전체를 채울 수 있는지 검사
-        // 3. 채울 수 없다면 돌아가서 그 다음 숫자를 대입 후 재시도
-        // 4. 처음으로 완성된 스도쿠(즉, 모든 비어있는 좌표에 값이 대입된 첫 시점)를 결과값으로 출력
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 1. 현재 3*3 공간에 1~9는 하나씩만 존재
+        // 2. 현재 행에 1~9는 하나씩만 존재
+        // 3. 현재 열에 1~9는 하나씩만 존재
+
         map = new int[9][9];
-        emptyAddr = new ArrayList<>();
-        isFinish = false;
-        
+        isDone = false;
         for(int i = 0; i < 9; i++) {
-            String[] sp = br.readLine().split("");
+            String[] st = br.readLine().split("");
             for(int j = 0; j < 9; j++) {
-                map[i][j] = Integer.parseInt(sp[j]);
-                if(map[i][j] == 0) {
-                    emptyAddr.add(new int[]{i, j}); // 1. 비어있는 좌표 수집
-                }
+                map[i][j] = Integer.parseInt(st[j]);
             }
         }
-        
-        dfs(0); // 백트래킹 이용
 
-        // 결과 출력
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }        
+        dfs(0);
     }
-
-    public static void dfs(int indx) {
-        if(indx == emptyAddr.size()) {
-            // 처음으로 스도쿠가 완성되면 연산 종료
-            isFinish = true;
+    public static void dfs(int depth) {
+        if(isDone == true) {
             return;
         }
- 
-        for(int i = 1; i <= 9; i++) {
-            int y = emptyAddr.get(indx)[0];
-            int x = emptyAddr.get(indx)[1];
-            if(checkNum(y, x, i)) {
-                map[y][x] = i;
-                dfs(indx + 1);
-                if(isFinish == true) {
-                    return;
+        if(depth == 81) {
+            isDone = true;
+            for(int i = 0; i < 9; i++) {
+                for(int j = 0; j < 9; j++) {
+                    System.out.print(map[i][j]);
                 }
+                System.out.println();
+            }
+            return;
+        }
+
+        int y = depth / 9;
+        int x = depth % 9;
+
+        if(map[y][x] != 0) {
+            dfs(depth + 1);
+            return;
+        }
+
+        for(int i = 1; i <= 9; i++) {
+            if(checkBlock(y, x, i) && checkRow(y, x, i) && checkCol(y, x, i)) {
+                map[y][x] = i;
+                dfs(depth + 1);
                 map[y][x] = 0;
             }
         }
-        
     }
-    
-    public static boolean checkNum(int y, int x, int target) {
-        // 1. 현재 속한 3*3 정사각형 그룹에서 대상 숫자가 이미 존재하는지 확인
-        // 2. 현재 열에서 대상 숫자가 이미 존재하는지 확인
-
-        for(int i = y/3*3; i < y/3*3+3; i++) {
-            for(int j = x/3*3; j < x/3*3+3; j++) {
-                if(map[i][j] == target) {
+    public static boolean checkBlock(int y, int x, int num) {
+        // 1. 현재 3*3 공간에 1~9는 하나씩만 존재하는지 검사
+        int blockY = y/3*3;
+        int blockX = x/3*3;
+        // System.out.println(blockY+" ~ "+(blockY+3));
+        // System.out.println(blockX+" ~ "+(blockX+3));
+        boolean[] check = new boolean[10]; // 1 ~ 9
+        check[num] = true;
+        for(int i = blockY; i < blockY + 3; i++) {
+            for(int j = blockX; j < blockX + 3; j++) {
+                int cur = map[i][j];
+                if(cur != 0 && check[cur] == true) {
                     return false;
+                } else {
+                    check[cur] = true;
                 }
             }
         }
-
+        return true;
+    }
+    public static boolean checkRow(int y, int x, int num) {
+        boolean[] check = new boolean[10]; // 1 ~ 9
+        check[num] = true;
         for(int i = 0; i < 9; i++) {
-            int curX = map[y][i];
-            int curY = map[i][x];
-            if(curX == target || curY == target) {
+            int cur = map[y][i];
+            if(cur != 0 && check[cur] == true) {
                 return false;
+            } else {
+                check[cur] = true;
             }
         }
-
+        return true;
+    }
+    public static boolean checkCol(int y, int x, int num) {
+        boolean[] check = new boolean[10]; // 1 ~ 9
+        check[num] = true;
+        for(int i = 0; i < 9; i++) {
+            int cur = map[i][x];
+            if(cur != 0 && check[cur] == true) {
+                return false;
+            } else {
+                check[cur] = true;
+            }
+        }
         return true;
     }
 }
